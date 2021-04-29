497c28113c86d080e34ce607e80dddf78c9c2979
Yali Wang <58898952+YaliWang2019@users.noreply.github.com>
2021-04-29 05:13:02 -0800

    Made basic selling program work.

---
+++ BoxOffice.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include "BoxOffice.hpp"
+using namespace std;
+
+void Customer::action()
+{
+	int num = 0;
+	std::cout << "Enter the number of purchase: \n" << endl;
+	std::cin >> num;
+	std::cout << "You successfully purchased " << num << " tickets of Godzilla vs. Kong.\n" << endl;
+	std::cout << "Paid $" << num * 10 << " in total.\n" << endl;
+}
+
+Conductor::Conductor(Customer* cur_Selling)
+{
+	std::cout << "Currently showing: \n" << endl;
+	std::cout << "Godzilla vs. Kong" << endl;
+	_selling = cur_Selling;
+}
+
+void Conductor::action()
+{
+	std::cout << "Each ticket is $10." << endl;
+	_selling -> action();
+}
+
+int main()
+{
+	Customer* _SubCustomer = new Customer();
+	Conductor* _ProConductor = new Conductor(_SubCustomer);
+	_ProConductor -> action();
+	delete _ProConductor;
+	delete _SubCustomer;
+	system("pause");
+	return 0;
+}
---
+++ BoxOffice.hpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+// Base class
+class Selling
+{
+public:
+	virtual void action() = 0;
+};
+
+// Customer class
+class Customer :public Selling
+{
+public:
+	void action() override;
+};
+
+// Conductor class
+class Conductor :public Selling
+{
+public:
+	Conductor(Customer* cur_Selling);
+	void action() override;
+private:
+	Customer* _selling;
+};
